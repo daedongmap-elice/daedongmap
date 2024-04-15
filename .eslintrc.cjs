fc876bf67e047ab44139a21a3ca6afca8f780348
Final Hyen <jwx9797@gmail.com>
2024-04-15 17:04:59 +0900

    lint test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,24 @@
 module.exports = {
   env: { browser: true, es2020: true, node: true },
   extends: [
+    "airbnb-typescript",
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:jsx-a11y/recommended",
     "plugin:prettier/recommended",
+    "prettier",
   ],
   parser: "@typescript-eslint/parser",
-  parserOptions: { ecmaVersion: "latest", sourceType: "module" },
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    project: ["./tsconfig.json"],
+  },
   plugins: [],
-  rules: {},
+  rules: {
+    "react/jsx-filename-extension": [
+      1,
+      { extensions: [".js", ".jsx", ".ts", "tsx"] },
+    ],
+  },
 };
