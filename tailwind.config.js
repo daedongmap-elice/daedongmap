fd79b0eef3e3f993e9f2f3416c1017c0df77c041
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-04-19 14:43:31 +0900

    feat: custom color 생성

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        mainY: "#FCDC2A",
+        mainG: "#2A6448",
+        YbtnText: "#3C3C3C",
+        GbtnText: "#FFFFFF",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };
