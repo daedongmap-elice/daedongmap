583050c4e67fd11eed9ac28b0c0cb3cf12e6fcea
onblana <51261847+onblana@users.noreply.github.com>
2024-04-19 19:03:11 +0900

    feat: 임시용 이미지 추가

--- src/pages/review/ReviewGallery.tsx
+++ src/pages/review/ReviewGallery.tsx
@@ -1,7 +1,8 @@
 import Logo from "../../components/review/ReviewGallery/Logo";
 import SelectLocation from "../../components/review/ReviewGallery/SelectLocation";
 import SelectSort from "../../components/review/ReviewGallery/SelectSort";
-import Photo from "../../components/review/Photo";
+import Thumbnail from "../../components/review/ReviewGallery/Thumbnail";
+import BottomNavbar from "../../components/BottomNavbar";
 
 export default function ReviewGallery() {
   return (
@@ -14,11 +15,64 @@ export default function ReviewGallery() {
         </div>
       </div>
       <div className="grid grid-cols-3 gap-px">
-        <Photo imgName="sample1.png" />
-        <Photo imgName="sample2.png" />
-        <Photo imgName="sample3.png" />
-        <Photo imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
+        <Thumbnail imgName="sample1.png" />
+        <Thumbnail imgName="sample2.png" />
+        <Thumbnail imgName="sample3.png" />
+        <Thumbnail imgName="sample4.png" />
       </div>
+      <BottomNavbar />
     </>
   );
 }
