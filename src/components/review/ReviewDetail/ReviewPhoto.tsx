c89921e736c45d43440c4dc4ad458446684d01c0
onblana <51261847+onblana@users.noreply.github.com>
2024-04-20 17:04:31 +0900

    feat: ReviewPhoto 퍼블리싱, carousel 추가

--- src/components/review/ReviewDetail/ReviewPhoto.tsx
+++ src/components/review/ReviewDetail/ReviewPhoto.tsx
@@ -1,7 +1,40 @@
 export default function ReviewPhoto() {
   return (
     <>
-      <div></div>
+      <div className="carousel w-full">
+        <div id="item1" className="carousel-item w-full">
+          <img src="img/sample3.png" className="w-full" alt="item" />
+        </div>
+        <div id="item2" className="carousel-item w-full">
+          <img src="img/sample4.png" className="w-full" alt="item" />
+        </div>
+        <div id="item3" className="carousel-item w-full">
+          <img src="img/sample1.png" className="w-full" alt="item" />
+        </div>
+        <div id="item4" className="carousel-item w-full">
+          <img src="img/sample2.png" className="w-full" alt="item" />
+        </div>
+        <div id="item5" className="carousel-item w-full">
+          <img src="img/sample3.png" className="w-full" alt="item" />
+        </div>
+      </div>
+      <div className="flex justify-center w-full py-2 gap-2">
+        <a href="#item1" className="btn btn-xs rounded-xl w-6">
+          1
+        </a>
+        <a href="#item2" className="btn btn-xs rounded-xl w-6">
+          2
+        </a>
+        <a href="#item3" className="btn btn-xs rounded-xl w-6">
+          3
+        </a>
+        <a href="#item4" className="btn btn-xs rounded-xl w-6">
+          4
+        </a>
+        <a href="#item" className="btn btn-xs rounded-xl w-6">
+          5
+        </a>
+      </div>
     </>
   );
 }
