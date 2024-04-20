63fc8bcc3db585aca317916d9e9c73e955e1584b
onblana <51261847+onblana@users.noreply.github.com>
2024-04-20 18:47:30 +0900

    feat: 별점 컴포넌트 퍼블리싱

---
+++ src/components/review/RatingStar.tsx
@@ -0,0 +1,33 @@
+export default function RatingStar() {
+  return (
+    <>
+      <div className="rating rating-sm">
+        <input
+          type="radio"
+          name="rating-6"
+          className="mask mask-star-2 bg-mainY"
+        />
+        <input
+          type="radio"
+          name="rating-6"
+          className="mask mask-star-2 bg-mainY"
+        />
+        <input
+          type="radio"
+          name="rating-6"
+          className="mask mask-star-2 bg-mainY"
+        />
+        <input
+          type="radio"
+          name="rating-6"
+          className="mask mask-star-2 bg-mainY"
+        />
+        <input
+          type="radio"
+          name="rating-6"
+          className="mask mask-star-2 bg-mainY"
+        />
+      </div>
+    </>
+  );
+}
