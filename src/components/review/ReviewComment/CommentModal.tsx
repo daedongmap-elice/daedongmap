1ba911ff45ee1f280f996ba96d11984f903a1241
onblana <51261847+onblana@users.noreply.github.com>
2024-04-20 19:48:34 +0900

    feat: CommentModal 퍼블리싱

---
+++ src/components/review/ReviewComment/CommentModal.tsx
@@ -0,0 +1,44 @@
+import CommentProfile from "./CommentProfile";
+
+export default function CommentModal() {
+  return (
+    <>
+      {/* You can open the modal using document.getElementById('ID').showModal() method */}
+      <button
+        className="btn"
+        onClick={() => document.getElementById("my_modal_3").showModal()}
+      >
+        open modal
+      </button>
+      <dialog id="my_modal_3" className="modal h-5/6 fixed bottom-10">
+        <div className="modal-box h-5/6 w-full rounded-b-none fixed bottom-0">
+          <form method="dialog">
+            {/* if there is a button in form, it will close the modal */}
+            <button className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">
+              ✕
+            </button>
+          </form>
+          <h3 className="font-bold text-base text-center">댓글</h3>
+          <p className="py-4 text-xs">
+            Press ESC key or click on ✕ button to close
+          </p>
+          <CommentProfile />
+          <CommentProfile />
+          <CommentProfile />
+          <div className="fixed bottom-5 w-10/12">
+            <form className="flex gap-2">
+              <input
+                type="text"
+                placeholder="댓글 달기..."
+                className="input input-bordered w-full h-8 text-xs"
+              />
+              <button className="btn btn-sm h-4 bg-mainG text-GbtnText text-xs">
+                확인
+              </button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </>
+  );
+}
