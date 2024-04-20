62678f52171103bbbca682102f0c3386c1b70baa
onblana <51261847+onblana@users.noreply.github.com>
2024-04-20 09:56:10 +0900

    design: 버튼 위치 수정

--- src/components/BottomNavbar.tsx
+++ src/components/BottomNavbar.tsx
@@ -3,26 +3,25 @@ import { BiSolidGridAlt } from "react-icons/bi";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { BiSolidUserCircle } from "react-icons/bi";
 
-/* bg-white */
 export default function BottomNavbar() {
   return (
     <div className="btm-nav h-9">
-      <button className="border-none rounded-none">
+      <button className="pb-0 border-none rounded-none bg-white">
         <BiSolidHome className="h-5 w-5" />
       </button>
-      <button className="border-none rounded-none bg-white">
+      <button className="pb-0 border-none rounded-none bg-white">
         <BiSolidGridAlt className="h-5 w-5" />
       </button>
-      <button className="border-none rounded-none bg-white">
-        <BsFillPlusCircleFill className="h-5 w-5" />
+      <button className="pb-0 border-none rounded-none bg-white">
+        <BsFillPlusCircleFill />
       </button>
-      <button className="border-none rounded-none bg-white">
-        <BiSolidUserCircle className="h-6 w-6" />
+      <button className="pb-0 border-none rounded-none bg-white">
+        <BiSolidUserCircle className="h-5 w-5" />
       </button>
     </div>
   );
 }
 
-// <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
-// <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
-// <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+// .btm-nav > *:not(.active) {
+//   padding-top: 0;
+// }
