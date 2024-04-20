cdb820e9becd19068f63501e0d4ef8788bb2b59d
onblana <51261847+onblana@users.noreply.github.com>
2024-04-20 11:28:34 +0900

    feat: 메인페이지에 하단Navbar 추가 (가려져서 안 보임)

--- src/pages/review/ReviewGallery.tsx
+++ src/pages/review/ReviewGallery.tsx
@@ -2,7 +2,6 @@ import Logo from "../../components/review/ReviewGallery/Logo";
 import SelectLocation from "../../components/review/ReviewGallery/SelectLocation";
 import SelectSort from "../../components/review/ReviewGallery/SelectSort";
 import Thumbnail from "../../components/review/ReviewGallery/Thumbnail";
-import BottomNavbar from "../../components/BottomNavbar";
 
 export default function ReviewGallery() {
   return (
@@ -72,7 +71,6 @@ export default function ReviewGallery() {
         <Thumbnail imgName="sample3.png" />
         <Thumbnail imgName="sample4.png" />
       </div>
-      <BottomNavbar />
     </>
   );
 }
