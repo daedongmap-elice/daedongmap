89b4214b95af96ea871a048c3a415a115db61dfa
Final Hyen <jwx9797@gmail.com>
2024-04-22 16:00:19 +0900

    feat: Access restriction function - 로그인 정보(쿠키) 유무에 따른 페이지 제한

--- src/components/layout/BottomNavbar.tsx
+++ src/components/layout/BottomNavbar.tsx
@@ -2,8 +2,10 @@ import { BiSolidHome } from "react-icons/bi";
 import { BiSolidGridAlt } from "react-icons/bi";
 import { BsFillPlusCircleFill } from "react-icons/bs";
 import { BiSolidUserCircle } from "react-icons/bi";
+import { getCookie } from "../../hooks/useCookie";
 
 export default function BottomNavbar() {
+  const getCookies = getCookie();
   return (
     <div className="btm-nav h-9">
       <button className="pb-0 border-none rounded-none bg-white">
@@ -17,12 +19,12 @@ export default function BottomNavbar() {
         </a>
       </button>
       <button className="pb-0 border-none rounded-none bg-white">
-        <a href="/post">
+        <a href={getCookies ? "/post" : "/login"}>
           <BsFillPlusCircleFill />
         </a>
       </button>
       <button className="pb-0 border-none rounded-none bg-white">
-        <a href="/mypage">
+        <a href={getCookies ? "/mypage" : "/prelogin"}>
           <BiSolidUserCircle className="h-5 w-5" />
         </a>
       </button>
