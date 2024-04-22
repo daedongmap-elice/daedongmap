9620714ba5c1e66a49922893611580342b9fccff
Final Hyen <jwx9797@gmail.com>
2024-04-22 16:29:19 +0900

    Docs: Image -assets 이미지 파일 public으로 이동

--- src/components/user/preLoginPresnt.tsx
+++ src/components/user/preLoginPresnt.tsx
@@ -1,11 +1,13 @@
-import icons from "./../../assests/LoginLogo.jpg";
-
 export default function PreLoginPresent() {
   return (
     <>
       <div className="w-full mt-[130px] flex flex-col items-center justify-center">
         <a href="/">
-          <img className="w-[275px] h-[290px]" src={icons} alt="로고 이미지" />
+          <img
+            className="w-[275px] h-[290px]"
+            src="img/LoginLogo.jpg"
+            alt="로고 이미지"
+          />
         </a>
         <a href="/signup">
           <button className="btn w-[235px] h-[40px] mb-[23px] bg-mainY text-YbtnText">
