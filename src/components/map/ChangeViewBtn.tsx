c2a8646777188b0f927220e9a09e2cb04eb4284d
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-04-22 15:54:36 +0900

    refactor: ChangeViewBtn 클릭시 발생되는 애니매이션 삭제

--- src/components/map/ChangeViewBtn.tsx
+++ src/components/map/ChangeViewBtn.tsx
@@ -2,9 +2,11 @@ import { FaListUl } from "react-icons/fa";
 
 export function ChangeViewBtn() {
   return (
-    <button className="btn btn-sm absolute bottom-10 left-1/2 z-10 -translate-x-1/2 rounded-full bg-white">
-      <FaListUl className="h-4 w-4"></FaListUl>
-      목록보기
-    </button>
+    <div className="absolute bottom-16 left-1/2 z-10 -translate-x-1/2">
+      <button className="btn btn-sm rounded-full bg-white">
+        <FaListUl className="h-4 w-4"></FaListUl>
+        목록보기
+      </button>
+    </div>
   );
 }
