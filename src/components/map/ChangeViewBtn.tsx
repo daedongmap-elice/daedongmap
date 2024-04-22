18971c60f64215c86ddcc3376f2353a27cfa948f
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-04-22 16:11:37 +0900

    style: ChangeViewBtn 스타일 변경

--- src/components/map/ChangeViewBtn.tsx
+++ src/components/map/ChangeViewBtn.tsx
@@ -3,7 +3,7 @@ import { FaListUl } from "react-icons/fa";
 export function ChangeViewBtn() {
   return (
     <div className="absolute bottom-16 left-1/2 z-10 -translate-x-1/2">
-      <button className="btn btn-sm rounded-full bg-white">
+      <button className="btn btn-sm rounded-full border-none bg-white shadow">
         <FaListUl className="h-4 w-4"></FaListUl>
         목록보기
       </button>
