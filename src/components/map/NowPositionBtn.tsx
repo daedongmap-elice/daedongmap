4ef21b2d462cd6b51ea424b5d173bf49e8b2e1e5
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-04-22 20:13:38 +0900

    refactor: MainMap, NowPositionBtn 구조 변경

--- src/components/map/NowPositionBtn.tsx
+++ src/components/map/NowPositionBtn.tsx
@@ -2,8 +2,10 @@ import { MdMyLocation } from "react-icons/md";
 
 export function NowPositionBtn() {
   return (
-    <button className="btn btn-circle absolute right-4 bottom-16 z-10 bg-white border-none btn-sm shadow">
-      <MdMyLocation className="h-4 w-4 text-mainG" />
-    </button>
+    <div className="absolute right-4 bottom-16 z-10">
+      <button className="btn btn-circle bg-white border-none btn-sm shadow">
+        <MdMyLocation className="h-4 w-4 text-mainG" />
+      </button>
+    </div>
   );
 }
