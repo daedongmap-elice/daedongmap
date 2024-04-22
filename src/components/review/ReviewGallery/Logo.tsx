32b374c00b3af4eaafadfa4de0d646d7baf0ee35
onblana <51261847+onblana@users.noreply.github.com>
2024-04-22 18:17:48 +0900

    style: 불필요한 fragment 제거

--- src/components/review/ReviewGallery/Logo.tsx
+++ src/components/review/ReviewGallery/Logo.tsx
@@ -1,7 +1,3 @@
 export default function Logo() {
-  return (
-    <>
-      <img className="inline w-28 ml-1" alt="logo" src="img/logo.png" />
-    </>
-  );
+  return <img className="inline w-28 ml-1" alt="logo" src="img/logo.png" />;
 }
