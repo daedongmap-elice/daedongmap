f48c000312408d59288f4e09d2c320bf424acbe4
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-04-22 14:03:01 +0900

    refactor: ChangeViewBtn path 태그 삭제

--- src/components/map/ChangeViewBtn.tsx
+++ src/components/map/ChangeViewBtn.tsx
@@ -3,14 +3,7 @@ import { FaListUl } from "react-icons/fa";
 export function ChangeViewBtn() {
   return (
     <button className="btn btn-sm rounded-full bg-white z-10 absolute bottom-10 left-1/2 -translate-x-1/2">
-      <FaListUl className="h-4 w-4">
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          stroke-width="2"
-          d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
-        />
-      </FaListUl>
+      <FaListUl className="h-4 w-4"></FaListUl>
       목록보기
     </button>
   );
