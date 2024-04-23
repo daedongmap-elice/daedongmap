4812bc87c1396fcbb80f3a850229d4732c29fb39
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-04-23 14:37:23 +0900

    feat: 마커 클릭시 PlaceInfoCard 보여지도록 적용

--- src/pages/MainMapPage.tsx
+++ src/pages/MainMapPage.tsx
@@ -5,14 +5,25 @@ import { ChangeViewBtn } from "../components/map/ChangeViewBtn";
 import { SearchInput } from "../components/map/SearchInput";
 import { NowPositionBtn } from "../components/map/NowPositionBtn";
 import { PlaceInfoCard } from "../components/map/PlaceInfoCard";
+import { useState } from "react";
 
 export function MainMapPage() {
+  const [selectMarker, setSelectMarker] = useState<{
+    lat: number;
+    lng: number;
+  }>({ lat: 0, lng: 0 });
+  // const [showInfoCard, setShowInfoCard] = useState<boolean>(false);
+
+  const handleOnClick = (position: { lat: number; lng: number }) => {
+    setSelectMarker(position);
+    // setShowInfoCard(true);
+  };
   return (
     <Map // 지도를 표시할 Container
       center={{
         // 지도의 중심좌표
-        lat: 37.54699,
-        lng: 127.09598,
+        lat: 37.4987872,
+        lng: 127.0289786,
       }}
       style={{
         // 지도의 크기
@@ -24,50 +35,88 @@ export function MainMapPage() {
       <MapMarker // 마커를 생성합니다
         position={{
           // 마커가 표시될 위치입니다
-          lat: 37.54699,
-          lng: 127.09598,
+          lat: 37.4987872,
+          lng: 127.0289786,
         }}
-        image={{
-          src: selectedMarkerIcon, // 마커이미지의 주소입니다
-          size: {
-            width: 26,
-            height: 32,
-          }, // 마커이미지의 크기입니다
-          options: {
-            offset: {
-              x: 13,
-              y: 32,
-            }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-          },
-        }}
+        image={
+          selectMarker.lat === 37.4987872 && selectMarker.lng === 127.0289786
+            ? {
+                src: selectedMarkerIcon, // 마커이미지의 주소입니다
+                size: {
+                  width: 26,
+                  height: 32,
+                }, // 마커이미지의 크기입니다
+                options: {
+                  offset: {
+                    x: 13,
+                    y: 32,
+                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                },
+              }
+            : {
+                src: markerIcon, // 마커이미지의 주소입니다
+                size: {
+                  width: 21,
+                  height: 29,
+                }, // 마커이미지의 크기입니다
+                options: {
+                  offset: {
+                    x: 10.5,
+                    y: 29,
+                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                },
+              }
+        }
+        onClick={() => handleOnClick({ lat: 37.4987872, lng: 127.0289786 })}
       />
       <MapMarker // 마커를 생성합니다
         position={{
           // 마커가 표시될 위치입니다
-          lat: 37.544,
-          lng: 127.094,
+          lat: 37.4994147,
+          lng: 127.0281479,
         }}
-        image={{
-          src: markerIcon, // 마커이미지의 주소입니다
-          size: {
-            width: 21,
-            height: 29,
-          }, // 마커이미지의 크기입니다
-          options: {
-            offset: {
-              x: 10.5,
-              y: 29,
-            }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-          },
-        }}
+        image={
+          selectMarker.lat === 37.4994147 && selectMarker.lng === 127.0281479
+            ? {
+                src: selectedMarkerIcon, // 마커이미지의 주소입니다
+                size: {
+                  width: 26,
+                  height: 32,
+                }, // 마커이미지의 크기입니다
+                options: {
+                  offset: {
+                    x: 13,
+                    y: 32,
+                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                },
+              }
+            : {
+                src: markerIcon, // 마커이미지의 주소입니다
+                size: {
+                  width: 21,
+                  height: 29,
+                }, // 마커이미지의 크기입니다
+                options: {
+                  offset: {
+                    x: 10.5,
+                    y: 29,
+                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+                },
+              }
+        }
+        onClick={() => handleOnClick({ lat: 37.4994147, lng: 127.0281479 })}
       />
       <SearchInput />
       <div>
         <ChangeViewBtn />
         <NowPositionBtn />
-        <div className="absolute bottom-16 z-10 w-[320px] left-1/2 -translate-x-1/2">
-          <PlaceInfoCard />
-        </div>
+        {selectMarker.lat !== 0 && selectMarker.lng !== 0 ? (
+          <div className="absolute bottom-16 z-10 w-[320px] left-1/2 -translate-x-1/2">
+            <PlaceInfoCard />
+          </div>
+        ) : (
+          <></>
+        )}
       </div>
     </Map>
   );
