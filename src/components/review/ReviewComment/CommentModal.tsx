2df9ef48c8b6137ce174055e2423321fabd818ee
onblana <51261847+onblana@users.noreply.github.com>
2024-04-24 23:09:12 +0900

    design: 댓글모달창 디자인 수정

--- src/components/review/ReviewComment/CommentModal.tsx
+++ src/components/review/ReviewComment/CommentModal.tsx
@@ -8,17 +8,19 @@ export default function CommentModal() {
         <Comment />
         <Comment />
         <Comment />
-        <div className="fixed bottom-5 w-10/12">
-          <form className="flex gap-2">
-            <input
-              type="text"
-              placeholder="댓글 달기..."
-              className="input input-bordered h-8 w-full text-xs"
-            />
-            <button className="btn btn-sm h-4 bg-mainG text-xs text-GbtnText">
-              확인
-            </button>
-          </form>
+        <div className="fixed bottom-7 w-full">
+          <div className="flex justify-center">
+            <form className="flex w-10/12 gap-2">
+              <input
+                type="text"
+                placeholder="댓글 달기..."
+                className="input input-bordered h-8 w-full text-xs"
+              />
+              <button className="btn btn-sm mr-2 h-4 bg-mainG text-xs text-GbtnText">
+                확인
+              </button>
+            </form>
+          </div>
         </div>
       </div>
       <form method="dialog" className="modal-backdrop">
