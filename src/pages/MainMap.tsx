1ff214166c1b4e46c692081a246d29da0daf067e
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-04-25 00:42:47 +0900

    feat: 마커를 클릭할 때만 placeInfoCard 보여주도록 설정 및 map() 메서드로 마커 자동 등록 및 주석 제거

--- src/pages/MainMap.tsx
+++ src/pages/MainMap.tsx
@@ -15,6 +15,41 @@ export function MainMap() {
   }>({ lat: 0, lng: 0 });
   const [showInfoCard, setShowInfoCard] = useState<boolean>(false);
   const [openListModal, setOpenListModal] = useState<boolean>(false);
+  const arr = [
+    {
+      place_name: "강남진해장",
+      place_url: "https://place.map.kakao.com/1428481536",
+      category_name: "한식",
+      address_name: "서울 강남구 역삼동 819-4",
+      road_address_name: "서울 강남구 테헤란로5길 11 유빌딩 1층",
+      id: "1428481536",
+      phone: "02-557-2662",
+      x: "127.0292471",
+      y: "37.4994553",
+    },
+    {
+      place_name: "갓덴스시 강남점",
+      place_url: "https://place.map.kakao.com/13575898",
+      category_name: "일식",
+      address_name: "서울 강남구 역삼동 822-4",
+      road_address_name: "서울 강남구 테헤란로 109 강남제일빌딩 1층",
+      id: "13575898",
+      phone: "02-2051-1477",
+      x: "127.0289786",
+      y: "37.4987872",
+    },
+    {
+      place_name: "용용선생 강남역점",
+      place_url: "https://place.map.kakao.com/1336266407",
+      category_name: "중식",
+      address_name: "서울 강남구 역삼동 817-21",
+      road_address_name: "서울 강남구 강남대로96길 17 1층",
+      id: "1336266407",
+      phone: "02-569-0999",
+      x: "127.0284191",
+      y: "37.5000464",
+    },
+  ];
 
   const handleOnClick = (position: { lat: number; lng: number }) => {
     setSelectMarker(position);
@@ -24,108 +59,101 @@ export function MainMap() {
   const handleOpenModal = () => {
     setOpenListModal(() => !openListModal);
   };
+
+  const handleClickMap = (
+    _: kakao.maps.Map,
+    moushEvent: kakao.maps.event.MouseEvent
+  ) => {
+    const latlng = moushEvent.latLng;
+    setSelectMarker({ lat: latlng.getLat(), lng: latlng.getLng() });
+    setShowInfoCard(false);
+  };
+
   return (
-    <Map // 지도를 표시할 Container
+    <Map
       center={{
-        // 지도의 중심좌표
         lat: 37.4987872,
         lng: 127.0289786,
       }}
       style={{
-        // 지도의 크기
         width: "100%",
         height: "95.3vh",
       }}
-      level={4} // 지도의 확대 레벨
+      level={4}
+      onClick={handleClickMap}
     >
-      <MapMarker // 마커를 생성합니다
-        position={{
-          // 마커가 표시될 위치입니다
-          lat: 37.4987872,
-          lng: 127.0289786,
-        }}
-        image={
-          selectMarker.lat === 37.4987872 && selectMarker.lng === 127.0289786
-            ? {
-                src: "svg/selectedMarker.svg", // 마커이미지의 주소입니다
-                size: {
-                  width: 26,
-                  height: 32,
-                }, // 마커이미지의 크기입니다
-                options: {
-                  offset: {
-                    x: 13,
-                    y: 32,
-                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-                },
-              }
-            : {
-                src: "svg/marker.svg", // 마커이미지의 주소입니다
-                size: {
-                  width: 21,
-                  height: 29,
-                }, // 마커이미지의 크기입니다
-                options: {
-                  offset: {
-                    x: 10.5,
-                    y: 29,
-                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-                },
-              }
-        }
-        onClick={() => handleOnClick({ lat: 37.4987872, lng: 127.0289786 })}
-      />
-      <MapMarker // 마커를 생성합니다
-        position={{
-          // 마커가 표시될 위치입니다
-          lat: 37.4994147,
-          lng: 127.0281479,
-        }}
-        image={
-          selectMarker.lat === 37.4994147 && selectMarker.lng === 127.0281479
-            ? {
-                src: "svg/selectedMarker.svg", // 마커이미지의 주소입니다
-                size: {
-                  width: 26,
-                  height: 32,
-                }, // 마커이미지의 크기입니다
-                options: {
-                  offset: {
-                    x: 13,
-                    y: 32,
-                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-                },
-              }
-            : {
-                src: "svg/marker.svg", // 마커이미지의 주소입니다
-                size: {
-                  width: 21,
-                  height: 29,
-                }, // 마커이미지의 크기입니다
-                options: {
-                  offset: {
-                    x: 10.5,
-                    y: 29,
-                  }, // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
-                },
-              }
-        }
-        onClick={() => handleOnClick({ lat: 37.4994147, lng: 127.0281479 })}
-      />
       <SearchInput />
-      <div>
-        <div
-          className={`absolute right-4 z-10 transition-all duration-150 ${showInfoCard ? "bottom-60" : "bottom-16"}`}
-        >
-          <NowPositionBtn />
-        </div>
-        {selectMarker.lat !== 0 && selectMarker.lng !== 0 ? (
-          <div className="absolute bottom-16 left-1/2 z-10 w-[320px] -translate-x-1/2">
-            <PlaceInfoCard />
-          </div>
-        ) : (
-          <></>
-        )}
+      {arr.map((place) => {
+        const {
+          // place_name: placeName,
+          // place_url: placeUrl,
+          // category_name: categoryName,
+          // address_name: addressName,
+          // road_address_name: roadAddressName,
+          // id,
+          // phone,
+          x: lng,
+          y: lat,
+        } = place;
+
+        return (
+          <>
+            <MapMarker
+              position={{
+                lat: Number(lat),
+                lng: Number(lng),
+              }}
+              image={
+                selectMarker.lat === Number(lat) &&
+                selectMarker.lng === Number(lng)
+                  ? {
+                      src: "svg/selectedMarker.svg",
+                      size: {
+                        width: 26,
+                        height: 32,
+                      },
+                      options: {
+                        offset: {
+                          x: 13,
+                          y: 32,
+                        },
+                      },
+                    }
+                  : {
+                      src: "svg/marker.svg",
+                      size: {
+                        width: 21,
+                        height: 29,
+                      },
+                      options: {
+                        offset: {
+                          x: 10.5,
+                          y: 29,
+                        },
+                      },
+                    }
+              }
+              onClick={() =>
+                handleOnClick({ lat: Number(lat), lng: Number(lng) })
+              }
+            />
+
+            {selectMarker.lat === Number(lat) &&
+            selectMarker.lng === Number(lng) ? (
+              <div className="absolute bottom-16 left-1/2 z-10 w-[320px] -translate-x-1/2">
+                <PlaceInfoCard />
+              </div>
+            ) : (
+              <></>
+            )}
+          </>
+        );
+      })}
+
+      <div
+        className={`absolute right-4 z-10 transition-all duration-150 ${showInfoCard ? "bottom-60" : "bottom-16"}`}
+      >
+        <NowPositionBtn />
       </div>
       <PlaceListModal openListModal={openListModal} />
       <div
