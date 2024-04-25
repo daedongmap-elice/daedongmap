9c76145995860066e0252e6fab167ecc64754239
onblana <51261847+onblana@users.noreply.github.com>
2024-04-25 19:43:08 +0900

    feat: 리뷰 등록 시 장소 선택 기능 작업 중

--- src/pages/review/ReviewPost.tsx
+++ src/pages/review/ReviewPost.tsx
@@ -1,17 +1,22 @@
-import { RatingStar, ImageInput } from "@/components/review/index";
+import { RatingStar, ImageInput, FindPlace } from "@/components/review/index";
+// import React, { useState } from "react";
+import axios from "axios";
 
 export default function ReviewPost() {
-  const handleSubmit = () => {
-    // e.preventDefault();
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post("/data", {
+        content: "content",
+      });
+      console.log("데이터 전송 성공:", response.data);
+    } catch (error) {
+      console.error("데이터 전송 실패:", error);
+    }
   };
-  const openSearch = () => {};
   return (
     <>
       <div className="mb-6 ml-5 mt-4 text-lg font-medium">새 리뷰 등록하기</div>
-      <form
-        onSubmit={handleSubmit}
-        className="flex flex-col items-center justify-center gap-3"
-      >
+      <div className="flex flex-col items-center justify-center gap-3">
         <div className="flex justify-center">
           <ImageInput />
         </div>
@@ -29,30 +34,41 @@ export default function ReviewPost() {
             <RatingStar />
           </div>
         </div>
-        <input
-          type="text"
-          placeholder="장소 선택"
-          className="input input-sm input-bordered w-3/4 max-w-xs flex-col text-xs"
-          onClick={openSearch}
-        />
-        <select className="select select-bordered select-sm w-3/4 max-w-xs text-xs">
-          <option disabled selected>
+        <button
+          type="button"
+          className="btn btn-outline btn-sm w-3/4 max-w-xs flex-col items-baseline border-gray-300 text-xs font-normal"
+          onClick={() => document.getElementById("placeModal").showModal()}
+        >
+          장소 선택
+        </button>
+        <dialog id="placeModal" className="modal modal-bottom">
+          <FindPlace />
+        </dialog>
+        <select
+          required
+          className="select select-bordered select-sm w-3/4 max-w-xs text-xs"
+        >
+          <option disabled defaultValue="default">
             분류 선택
           </option>
-          <option>한식</option>
-          <option>중식</option>
-          <option>일식</option>
-          <option>양식</option>
-          <option>기타</option>
+          <option value="korean">한식</option>
+          <option value="chinese">중식</option>
+          <option value="japanese">일식</option>
+          <option value="Western">양식</option>
+          <option value="other">기타</option>
         </select>
         <textarea
+          required
           className="textarea textarea-bordered h-32 w-3/4 max-w-xs pt-3 text-xs"
           placeholder="리뷰 내용 입력..."
         ></textarea>
-        <button className="btn btn-sm mt-2 w-1/2 bg-mainY font-medium text-YbtnText">
+        <button
+          onClick={handleSubmit}
+          className="btn btn-sm mt-2 w-1/2 bg-mainY font-medium text-YbtnText"
+        >
           리뷰 등록
         </button>
-      </form>
+      </div>
     </>
   );
 }
