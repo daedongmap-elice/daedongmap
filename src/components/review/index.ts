3d7e327f342d0374d29693f11429740427f27634
onblana <51261847+onblana@users.noreply.github.com>
2024-04-28 17:57:52 +0900

    rename: .tsx를 .ts로 수정 (jsx 사용안함)

--- src/components/review/index.ts
+++ src/components/review/index.ts
@@ -1,5 +1,5 @@
 // export 할 컴포넌트들을 index파일에 한꺼번에 정리
-// 유지보수 차원에서 좋음 (파일명 변경 시 import하는 모든 곳에서 변경해야 하지만, index(색인)가 있으면 여기만 수정하면 됨)
+// 유지보수 차원에서 좋음 (파일명 변경 시 import하는 모든 곳에서 변경해야 하지만, index가 있으면 여기만 수정하면 됨)
 
 export { default as Comment } from "./ReviewComment/Comment";
 export { default as CommentModal } from "./ReviewComment/CommentModal";
