41ff485a0d95402ff670f496b572d4b9ab8a0baf
onblana <51261847+onblana@users.noreply.github.com>
2024-04-28 17:59:49 +0900

    refactor: 직접적인 DOM조작 코드 변경 (handleSeeMore함수 삭제)

--- src/pages/review/ReviewDetail.tsx
+++ src/pages/review/ReviewDetail.tsx
@@ -7,17 +7,10 @@ import {
   ReviewProfile,
   CommentModal,
 } from "@/components/review/index";
+import { useState } from "react";
 
 export default function ReviewDetail() {
-  const handleSeeMore = (event: React.MouseEvent) => {
-    const button = event.target as HTMLButtonElement;
-    button.style.display = "none";
-
-    const content = document.getElementById("content") as HTMLDivElement;
-    content.style.overflow = "visible";
-    content.style.whiteSpace = "normal";
-    content.style.textOverflow = "initial";
-  };
+  const [isSeeMoreClicked, setIsSeeMoreClicked] = useState(false);
 
   return (
     <>
@@ -47,20 +40,24 @@ export default function ReviewDetail() {
         </div>
       </div>
       <div className="flex w-full justify-between px-5 pt-4 text-sm">
-        <p id="content" className="overflow-hidden text-clip whitespace-nowrap">
+        <p
+          id="content"
+          className={`${isSeeMoreClicked ? "" : "text-clip"} overflow-${isSeeMoreClicked ? "visible" : "hidden"} whitespace-${isSeeMoreClicked ? "normal" : "nowrap"}`}
+        >
           오늘 여기 식당에서 점심 먹었는데 제육덮밥 맛있었어요! 헌법개정안은
           국회가 의결한 후 30일 이내에 국민투표에 붙여 국회의원선거권자 과반수의
           투표와 투표자 과반수의 찬성을 얻어야 한다.
         </p>
         <button
-          onClick={handleSeeMore}
-          className="min-w-fit cursor-pointer text-subGray"
+          onClick={() => setIsSeeMoreClicked(true)}
+          className={`min-w-fit cursor-pointer text-subGray ${isSeeMoreClicked ? "hidden" : ""}`}
         >
           <span>...&nbsp; 더 보기</span>
         </button>
       </div>
       <div className="mb-6 px-5 pt-2 text-sm text-subGray">
         <button
+          // @ts-expect-error NOTE: DaisyUI의 Modal 사용을 위함
           onClick={() => document.getElementById("commentModal").showModal()}
         >
           댓글 0개 보기
