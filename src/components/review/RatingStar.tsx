d79b465d2a27b45f2a34be8aed122347cce6f629
onblana <51261847+onblana@users.noreply.github.com>
2024-04-29 16:53:55 +0900

    fix: 별점이 항목별로 개별 동작하도록 수정

--- src/components/review/RatingStar.tsx
+++ src/components/review/RatingStar.tsx
@@ -1,33 +1,44 @@
 interface RatingStarProps {
-  item: string;
+  name: string;
+  setRating: React.Dispatch<React.SetStateAction<number>>;
 }
 
-const RatingStar: React.FC<RatingStarProps> = ({ item }) => {
+const RatingStar: React.FC<RatingStarProps> = ({ name, setRating }) => {
   return (
-    <div id={item} className="rating rating-sm">
+    <div className="rating rating-sm">
       <input
         type="radio"
-        name="rating-6"
+        name={name}
+        value={1}
+        onChange={() => setRating(1)}
         className="mask mask-star-2 bg-mainY"
       />
       <input
         type="radio"
-        name="rating-6"
+        name={name}
+        value={2}
+        onChange={() => setRating(2)}
         className="mask mask-star-2 bg-mainY"
       />
       <input
         type="radio"
-        name="rating-6"
+        name={name}
+        value={3}
+        onChange={() => setRating(3)}
         className="mask mask-star-2 bg-mainY"
       />
       <input
         type="radio"
-        name="rating-6"
+        name={name}
+        value={4}
+        onChange={() => setRating(4)}
         className="mask mask-star-2 bg-mainY"
       />
       <input
         type="radio"
-        name="rating-6"
+        name={name}
+        value={5}
+        onChange={() => setRating(5)}
         className="mask mask-star-2 bg-mainY"
       />
     </div>
