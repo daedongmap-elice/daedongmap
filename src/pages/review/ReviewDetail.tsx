c9e50a8f7afa6b2e89a27fe8c1944f734edf4e56
onblana <51261847+onblana@users.noreply.github.com>
2024-05-02 23:40:07 +0900

    feat: respons의 type 설정, 본문내용content 표시, useEffect 추가

--- src/pages/review/ReviewDetail.tsx
+++ src/pages/review/ReviewDetail.tsx
@@ -7,27 +7,64 @@ import {
   ReviewProfile,
   CommentModal,
 } from "@/components/review/index";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-export default function ReviewDetail() {
+interface ReviewDetailResponse {
+  id: number;
+  kakaoPlaceId: number;
+  placeName: string;
+  user: {
+    id: number;
+    nickName: string;
+    email: string;
+  };
+  content: string;
+  reviewImageDtoList: [
+    {
+      id: number;
+      userId: number;
+      reviewId: number;
+      filePath: string;
+    },
+  ];
+  tasteRating: number;
+  hygieneRating: number;
+  kindnessRating: number;
+  averageRating: number;
+  likeCount: number;
+  createdAt: string | undefined;
+  updatedAt: string;
+}
+
+// TODO: 페이지 진입 시 /api/review/detail과 /api/like에 get요청
+//       데이터 받아서 ReviewProfile, ReviewImage, DateCreated, Star에 뿌리기
+const ReviewDetail = () => {
   const [isSeeMoreClicked, setIsSeeMoreClicked] = useState(false);
   const [isLiked, setIsLiked] = useState(false); // TODO: GET요청의 결과를 초기값으로 지정
+  const [data, setData] = useState<ReviewDetailResponse | null>(null);
+  const getData = async () => {
+    try {
+      const response = await axios.get(
+        "http://35.232.243.53:8080/api/reviews/4"
+      );
+      setData(response.data);
+    } catch (error) {
+      console.error("리뷰상세 get요청 에러", error);
+    }
+  };
 
-  // TO FIX: text-clip 클래스 적용 안 됨(더보기버튼클릭전에도 글 내용이 다 보임)
+  useEffect(() => {
+    getData();
+  }, []);
 
   // TODO: ReviewProfile 클릭 시 해당 유저의 '마이페이지'로 이동
   // TODO: EditButton 클릭 시 ReviewEdit으로 가야 함
-  // TODO: 페이지 진입 시 /api/review/detail과 /api/like에 get요청 보내기
-  // 데이터 받아서 ReviewProfile, ReviewImage, DateCreated, Star에 뿌리기
-  // base64 형식을 디코딩하여 이미지로 나타내기
 
   // TODO: EditButton에서 수정 버튼 클릭 시 현재 리뷰아이디를 ReviewEdit에 전달하기
   // TODO: EditButton에서 삭제 버튼 클릭 시 현재 리뷰아이디로 delete요청 보내기
   //      '리뷰를 삭제하시겠습니까?' 모달 띄우고 확인/취소 버튼
   // TODO: 로그인되어있는 userID와 현재 글의 userID가 일치할 경우에만 EditButton 표시
-
-  // TODO: 댓글 보기 클릭 시 CommentModal에서 댓글api에 get요청 보내고 데이터 뿌리기
-  // TODO: CommentModal에서도 각 댓글에서 로그인userID와 댓글userID가 일치할 경우에만 EditButton 표시
 
   // TODO: 페이지 진입 시 ‘나의 좋아요 클릭 여부’와 '해당 리뷰의 좋아요 개수'를 조회(GET) 후
   //       반영 - isLiked 여부, N명이 좋아합니다
@@ -36,7 +73,11 @@ export default function ReviewDetail() {
   return (
     <div className="pb-16">
       <div className="flex items-center justify-between">
-        <ReviewProfile />
+        <ReviewProfile
+          userId={data?.user.id}
+          nickName={data?.user.nickName}
+          placeName={data?.placeName}
+        />
         <div className="mb-3 mr-3 mt-4">
           <EditButton />
         </div>
@@ -44,29 +85,27 @@ export default function ReviewDetail() {
       <ReviewImage />
       <div className="mt-2 flex items-center justify-between">
         <LikeButton isLiked={isLiked} setIsLiked={setIsLiked} />
-        <DateCreated />
+        <DateCreated createdAt={data?.createdAt} />
       </div>
       <div className="mt-3 flex items-center justify-between pl-5 pr-5 text-sm">
         <div className="flex items-center gap-1">
           <span className="min-w-fit">맛</span>
-          <Star name="taste" rating={3} />
+          <Star name="taste" rating={data?.tasteRating} />
         </div>
         <div className="flex items-center gap-1">
           <span className="min-w-fit">위생</span>
-          <Star name="hygiene" rating={2} />
+          <Star name="hygiene" rating={data?.hygieneRating} />
         </div>
         <div className="flex items-center gap-1">
           <span className="min-w-fit">친절</span>
-          <Star name="kindness" rating={5} />
+          <Star name="kindness" rating={data?.kindnessRating} />
         </div>
       </div>
       <div className="flex w-full justify-between px-5 pt-4 text-sm">
         <p
-          className={`${isSeeMoreClicked ? "" : "text-clip"} overflow-${isSeeMoreClicked ? "visible" : "hidden"} whitespace-${isSeeMoreClicked ? "normal" : "nowrap"}`}
+          className={`${isSeeMoreClicked ? "overflow-visible" : "overflow-hidden"} ${isSeeMoreClicked ? "whitespace-normal" : "whitespace-nowrap"} ${isSeeMoreClicked ? "" : "text-clip"}`}
         >
-          오늘 여기 식당에서 점심 먹었는데 제육덮밥 맛있었어요! 헌법개정안은
-          국회가 의결한 후 30일 이내에 국민투표에 붙여 국회의원선거권자 과반수의
-          투표와 투표자 과반수의 찬성을 얻어야 한다.
+          {data?.content}
         </p>
 
         <button
@@ -89,4 +128,6 @@ export default function ReviewDetail() {
       </div>
     </div>
   );
-}
+};
+
+export default ReviewDetail;
