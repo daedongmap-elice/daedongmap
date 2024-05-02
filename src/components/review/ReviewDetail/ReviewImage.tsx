c9daafe65e1725189fecc7137803a57326140ea7
onblana <51261847+onblana@users.noreply.github.com>
2024-05-03 01:16:34 +0900

    feat: 서버로부터 받은 이미지 표시 기능 추가

--- src/components/review/ReviewDetail/ReviewImage.tsx
+++ src/components/review/ReviewDetail/ReviewImage.tsx
@@ -1,127 +1,69 @@
 import { useState } from "react";
 
-export default function ReviewImage() {
-  // TODO: 해당 게시글의 이미지를 조회해서 불러온 다음 map으로 그려내야 함
-  //       base64형식의 string배열을 FileReader API를 통해 File 형식으로 변환해야 함
-  // TODO: 버튼으로 한장씩 넘기도록, 페이지 나타내는 것 번호를 작은 원으로 고치고 버튼없애기
+interface ReviewImageProps {
+  fileLinks: string[];
+}
+
+const ReviewImage: React.FC<ReviewImageProps> = ({ fileLinks }) => {
   const [slideIndex, setSlideIndex] = useState(1);
 
   return (
     <>
       <div className="carousel w-full">
-        <div id="slide1" className="carousel-item relative w-full">
-          <img src="img/sample3.png" className="w-full" alt="item" />
-          <div className="absolute left-1 right-1 top-1/2 flex -translate-y-1/2 transform justify-between opacity-50">
-            <a
-              href="/"
-              className="btn btn-circle invisible"
-              onClick={() => setSlideIndex(1)}
-            >
-              ❮
-            </a>
-            <a
-              href="#slide2"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(2)}
-            >
-              ❯
-            </a>
+        {fileLinks.map((link, i) => (
+          <div
+            id={`slide${i}`}
+            key={`slide${i}`}
+            className="carousel-item relative w-full"
+          >
+            {fileLinks.length === 1 ? (
+              <img src={link} className="w-full" alt="item" />
+            ) : (
+              <>
+                <img src={link} className="w-full" alt="item" />
+                <div className="absolute left-1 right-1 top-1/2 flex -translate-y-1/2 transform justify-between opacity-50">
+                  <a
+                    href={i === 0 ? "/" : `#slide${i - 1}`}
+                    className={`btn btn-circle ${i === 0 ? "invisible" : ""}`}
+                    onClick={() =>
+                      i === 0 ? setSlideIndex(0) : setSlideIndex(i - 1)
+                    }
+                  >
+                    ❮
+                  </a>
+                  <a
+                    href={i === fileLinks.length - 1 ? "/" : `#slide${i + 1}`}
+                    className={`btn btn-circle ${i === fileLinks.length - 1 ? "invisible" : ""}`}
+                    onClick={() =>
+                      i === fileLinks.length - 1
+                        ? setSlideIndex(fileLinks.length - 1)
+                        : setSlideIndex(i + 1)
+                    }
+                  >
+                    ❯
+                  </a>
+                </div>
+              </>
+            )}
           </div>
-        </div>
-        <div id="slide2" className="carousel-item relative w-full">
-          <img src="img/sample4.png" className="w-full" alt="item" />
-          <div className="absolute left-1 right-1 top-1/2 flex -translate-y-1/2 transform justify-between opacity-50">
-            <a
-              href="#slide1"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(1)}
-            >
-              ❮
-            </a>
-            <a
-              href="#slide3"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(3)}
-            >
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide3" className="carousel-item relative w-full">
-          <img src="img/sample1.png" className="w-full" alt="item" />
-          <div className="absolute left-1 right-1 top-1/2 flex -translate-y-1/2 transform justify-between opacity-50">
-            <a
-              href="#slide2"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(2)}
-            >
-              ❮
-            </a>
-            <a
-              href="#slide4"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(4)}
-            >
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide4" className="carousel-item relative w-full">
-          <img src="img/sample2.png" className="w-full" alt="item" />
-          <div className="absolute left-1 right-1 top-1/2 flex -translate-y-1/2 transform justify-between opacity-50">
-            <a
-              href="#slide3"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(3)}
-            >
-              ❮
-            </a>
-            <a
-              href="#slide5"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(5)}
-            >
-              ❯
-            </a>
-          </div>
-        </div>
-        <div id="slide5" className="carousel-item relative w-full">
-          <img src="img/sample4.png" className="w-full" alt="item" />
-          <div className="absolute left-1 right-1 top-1/2 flex -translate-y-1/2 transform justify-between opacity-50">
-            <a
-              href="#slide4"
-              className="btn btn-circle"
-              onClick={() => setSlideIndex(4)}
-            >
-              ❮
-            </a>
-            <a
-              href="/"
-              className="btn btn-circle invisible"
-              onClick={() => setSlideIndex(5)}
-            >
-              ❯
-            </a>
-          </div>
-        </div>
+        ))}
       </div>
-      <div className="flex w-full items-center justify-center gap-1 pt-3">
-        <div
-          className={`rounded ${slideIndex === 1 ? "h-1.5 w-1.5 bg-mainG" : "h-1 w-1 rounded bg-subLightGray"}`}
-        ></div>
-        <div
-          className={`rounded ${slideIndex === 2 ? "h-1.5 w-1.5 bg-mainG" : "h-1 w-1 rounded bg-subLightGray"}`}
-        ></div>
-        <div
-          className={`rounded ${slideIndex === 3 ? "h-1.5 w-1.5 bg-mainG" : "h-1 w-1 rounded bg-subLightGray"}`}
-        ></div>
-        <div
-          className={`rounded ${slideIndex === 4 ? "h-1.5 w-1.5 bg-mainG" : "h-1 w-1 rounded bg-subLightGray"}`}
-        ></div>
-        <div
-          className={`rounded ${slideIndex === 5 ? "h-1.5 w-1.5 bg-mainG" : "h-1 w-1 rounded bg-subLightGray"}`}
-        ></div>
+      <div
+        id="slideIndex"
+        className="flex w-full items-center justify-center gap-1 pt-3"
+      >
+        {fileLinks.map((link, i) =>
+          fileLinks.length === 1 ? (
+            <></>
+          ) : (
+            <div
+              className={`rounded ${slideIndex === i ? "h-1.5 w-1.5 bg-mainG" : "h-1 w-1 rounded bg-subLightGray"}`}
+            ></div>
+          )
+        )}
       </div>
     </>
   );
-}
+};
+
+export default ReviewImage;
