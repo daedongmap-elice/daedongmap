1b6222cdf274908a119951e31f204c539e3d049d
onblana <51261847+onblana@users.noreply.github.com>
2024-05-04 04:00:12 +0900

    design: 리뷰갤러리 상단 로고 수정

--- src/components/review/ReviewGallery/Logo.tsx
+++ src/components/review/ReviewGallery/Logo.tsx
@@ -1,3 +1,5 @@
 export default function Logo() {
-  return <img className="ml-1 inline w-28" alt="logo" src="img/logo.png" />;
+  return (
+    <img className="ml-2 mt-2 inline w-28" alt="logo" src="svg/logo.svg" />
+  );
 }
