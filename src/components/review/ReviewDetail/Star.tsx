2cf8888ce476adb57da65f663968525d33c2a94a
onblana <51261847+onblana@users.noreply.github.com>
2024-05-05 15:59:10 +0900

    refactor: 불필요한 삼항연산자 제거

--- src/components/review/ReviewDetail/Star.tsx
+++ src/components/review/ReviewDetail/Star.tsx
@@ -10,35 +10,35 @@ const Star: React.FC<StarProps> = ({ name, rating }) => {
         type="radio"
         name={name}
         readOnly
-        checked={rating === 1 ? true : false}
+        checked={rating === 1}
         className="mask mask-star-2 pointer-events-none bg-mainY"
       />
       <input
         type="radio"
         name={name}
         readOnly
-        checked={rating === 2 ? true : false}
+        checked={rating === 2}
         className="mask mask-star-2 pointer-events-none bg-mainY"
       />
       <input
         type="radio"
         name={name}
         readOnly
-        checked={rating === 3 ? true : false}
+        checked={rating === 3}
         className="mask mask-star-2 pointer-events-none bg-mainY"
       />
       <input
         type="radio"
         name={name}
         readOnly
-        checked={rating === 4 ? true : false}
+        checked={rating === 4}
         className="mask mask-star-2 pointer-events-none bg-mainY"
       />
       <input
         type="radio"
         name={name}
         readOnly
-        checked={rating === 5 ? true : false}
+        checked={rating === 5}
         className="mask mask-star-2 pointer-events-none bg-mainY"
       />
     </div>
