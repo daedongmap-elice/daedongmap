bc67f5d90acfcf6b7da5fcce6503380fca025350
onblana <51261847+onblana@users.noreply.github.com>
2024-05-06 22:16:52 +0900

    feat: 좋아요 개수 반영 기능 추가

--- src/components/review/ReviewDetail/LikeButton.tsx
+++ src/components/review/ReviewDetail/LikeButton.tsx
@@ -3,9 +3,14 @@ import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
 interface LikeProps {
   isLiked: boolean;
   setIsLiked: React.Dispatch<React.SetStateAction<boolean>>;
+  likeCount: number;
 }
 
-const LikeButton: React.FC<LikeProps> = ({ isLiked, setIsLiked }) => {
+const LikeButton: React.FC<LikeProps> = ({
+  isLiked,
+  setIsLiked,
+  likeCount,
+}) => {
   return (
     <div className="flex items-center">
       <button
@@ -19,7 +24,9 @@ const LikeButton: React.FC<LikeProps> = ({ isLiked, setIsLiked }) => {
           <AiOutlineHeart className="ml-3 h-8 w-8" />
         )}
       </button>
-      <span className="ml-1 text-sm text-subGray">0명이 좋아합니다</span>
+      <span className="ml-1 text-sm text-subGray">
+        {likeCount}명이 좋아합니다
+      </span>
     </div>
   );
 };
