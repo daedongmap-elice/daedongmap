10b41dc1a97e6e88d91c80e5e8a3c36ec45a627e
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-05-08 14:02:52 +0900

    feat: searchInput 텍스트 삭제 버튼 추가

--- src/components/map/SearchInput.tsx
+++ src/components/map/SearchInput.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { IoSearch } from "react-icons/io5";
+import { IoSearch, IoCloseCircle } from "react-icons/io5";
 import Toast from "../common/Toast";
 
 interface SearchInputProps {
@@ -20,101 +20,112 @@ export default function SearchInput({
   handleSetMarkers,
 }: SearchInputProps) {
   const [text, setText] = useState<string>("");
-  const [toast, setToast] = useState<boolean>(false);
+  const [emptyInputToast, setEmptyInputToast] = useState<boolean>(false);
+  const [zeroResultToast, setZeroResultToast] = useState<boolean>(false);
 
   const handleOnClick = (e: React.ChangeEvent<HTMLInputElement>) => {
     setText(e.target.value);
   };
 
-  const handleSearch = () => {
+  const handleResetText = () => {
+    setText("");
+  };
+
+  const handleSearchPlace = () => {
     if (!map) return;
     const ps = new kakao.maps.services.Places();
 
-    if (text !== "") {
-      try {
-        ps.keywordSearch(`${text} 음식점`, (datas, status) => {
-          if (status === kakao.maps.services.Status.ZERO_RESULT) {
-            setToast(true);
-          }
-          if (status === kakao.maps.services.Status.OK) {
-            if (type === "main") {
-              map.setCenter(
-                new kakao.maps.LatLng(Number(datas[0].y), Number(datas[0].x))
-              );
-              map.setLevel(5);
-              if (getPlaces) {
-                getPlaces();
-              }
+    try {
+      ps.keywordSearch(`${text} 음식점`, (datas, status) => {
+        if (status === kakao.maps.services.Status.ZERO_RESULT) {
+          setZeroResultToast(true);
+        }
+        if (status === kakao.maps.services.Status.OK) {
+          if (type === "main") {
+            map.setCenter(
+              new kakao.maps.LatLng(Number(datas[0].y), Number(datas[0].x))
+            );
+            map.setLevel(5);
+            if (getPlaces) {
+              getPlaces();
             }
+          }
 
-            if (type === "post") {
-              const bounds = new kakao.maps.LatLngBounds();
-              const newMarkers: {
-                kakaoPlaceId: number;
-                placeName: string;
-                addressName: string;
-                categoryName: string;
-                roadAddressName: string;
-                placeUrl: string;
-                phone: string | null;
-                x: number;
-                y: number;
-              }[] = [];
+          if (type === "post") {
+            const bounds = new kakao.maps.LatLngBounds();
+            const newMarkers: {
+              kakaoPlaceId: number;
+              placeName: string;
+              addressName: string;
+              categoryName: string;
+              roadAddressName: string;
+              placeUrl: string;
+              phone: string | null;
+              x: number;
+              y: number;
+            }[] = [];
 
-              datas.map((data) => {
-                const {
-                  address_name: addressName,
-                  category_name: categoryName,
-                  id: kakaoPlaceId,
-                  phone,
-                  place_name: placeName,
-                  place_url: placeUrl,
-                  road_address_name: roadAddressName,
-                  x,
-                  y,
-                } = data;
-                const category = categoryName.split(">")[1].trim();
+            datas.map((data) => {
+              const {
+                address_name: addressName,
+                category_name: categoryName,
+                id: kakaoPlaceId,
+                phone,
+                place_name: placeName,
+                place_url: placeUrl,
+                road_address_name: roadAddressName,
+                x,
+                y,
+              } = data;
+              const category = categoryName.split(">")[1].trim();
 
-                newMarkers.push({
-                  addressName,
-                  categoryName: category,
-                  kakaoPlaceId: Number(kakaoPlaceId),
-                  phone,
-                  placeName,
-                  placeUrl,
-                  roadAddressName,
-                  x: Number(x),
-                  y: Number(y),
-                });
-
-                bounds.extend(new kakao.maps.LatLng(Number(y), Number(x)));
+              newMarkers.push({
+                addressName,
+                categoryName: category,
+                kakaoPlaceId: Number(kakaoPlaceId),
+                phone,
+                placeName,
+                placeUrl,
+                roadAddressName,
+                x: Number(x),
+                y: Number(y),
               });
 
-              if (handleSetMarkers) {
-                handleSetMarkers(newMarkers);
-              }
+              bounds.extend(new kakao.maps.LatLng(Number(y), Number(x)));
+            });
 
-              map.setBounds(bounds);
+            if (handleSetMarkers) {
+              handleSetMarkers(newMarkers);
             }
+
+            map.setBounds(bounds);
           }
-        });
-      } catch (err) {
-        console.log(err);
-      }
-      handleResetSelectMarker();
-      handleToggleShowInfoCard(false);
+        }
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    handleResetSelectMarker();
+    handleToggleShowInfoCard(false);
+  };
+
+  const handlerOnClickSearch = () => {
+    if (text === "") {
+      setEmptyInputToast(true);
+    } else {
+      handleSearchPlace();
     }
   };
 
   const handleOnKeyDown = (e: React.KeyboardEvent) => {
     if (e.key === "Enter") {
-      handleSearch();
+      handlerOnClickSearch();
       (e.currentTarget as HTMLInputElement).blur();
     }
   };
 
   useEffect(() => {
-    handleSearch();
+    handleSearchPlace();
   }, [map]);
 
   return (
@@ -123,21 +134,34 @@ export default function SearchInput({
         <div className="relative h-11 w-[320px]">
           <input
             type="text"
+            value={text}
             className="absolute h-full w-full rounded-md p-2.5 text-sm shadow"
             placeholder={type === "main" ? "지역 검색" : "음식점 검색"}
             onChange={(e) => handleOnClick(e)}
             onKeyDown={(e) => handleOnKeyDown(e)}
           ></input>
+          {text !== "" && (
+            <IoCloseCircle
+              onClick={handleResetText}
+              className="absolute right-10 top-1/2 h-4 w-4 -translate-y-1/2 text-subLightGray"
+            />
+          )}
           <IoSearch
-            onClick={handleSearch}
+            onClick={handlerOnClickSearch}
             className="absolute right-3 top-1/2 h-5 w-5 -translate-y-1/2"
           />
         </div>
       </div>
-      {toast && (
+      {zeroResultToast && (
         <Toast
-          setToast={setToast}
+          setToast={setZeroResultToast}
           message={`"${text}"(으)로 검색한 결과가 없습니다.`}
+        />
+      )}
+      {emptyInputToast && (
+        <Toast
+          setToast={setEmptyInputToast}
+          message={"검색어를 입력해주세요."}
         />
       )}
     </>
