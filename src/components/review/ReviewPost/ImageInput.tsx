7dacf67f11d7d1af61c33203da8dcf8dc3ac71a5
onblana <51261847+onblana@users.noreply.github.com>
2024-05-08 23:59:39 +0900

    fix: 이미지 전부 삭제 시 파일첨부창이 자동으로 뜨는 이슈 해결

--- src/components/review/ReviewPost/ImageInput.tsx
+++ src/components/review/ReviewPost/ImageInput.tsx
@@ -38,10 +38,10 @@ const ImageInput = ({ prevImgUrls, handlePostImgs }: ImageInputProps) => {
   }, [prevImgUrls]);
 
   return (
-    <div className="flex flex-col items-center justify-center gap-2">
+    <div className="relative flex flex-col items-center justify-center gap-2">
       {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
       {/* label태그 내에 div를 넣었을 때의 오류 해결을 위함 */}
-      <label htmlFor="file">
+      <label htmlFor="file" className="absolute top-0 h-52 w-52">
         <input
           multiple
           className="hidden"
@@ -52,63 +52,59 @@ const ImageInput = ({ prevImgUrls, handlePostImgs }: ImageInputProps) => {
           type="file"
           name="file"
         />
-        <div className="carousel h-52 w-52 border border-solid border-gray-300">
-          {previewImgs.map((url, i) => (
-            <div
-              key={url}
-              id={`item${i}`}
-              className="carousel-item relative w-full overflow-hidden"
+        <div
+          className={`${previewImgs.length > 0 ? "hidden" : ""} carousel-item flex h-full w-full flex-col items-center justify-center gap-1 bg-subLightGray text-base text-subGray`}
+        >
+          사진 추가
+          <div className="text-xs">1MB 미만의 파일만 업로드 가능합니다</div>
+          <div className="text-xs">(최대 5장까지 첨부 가능)</div>
+        </div>
+      </label>
+      <div className="carousel h-52 w-52 border border-solid border-gray-300">
+        {previewImgs.map((url, i) => (
+          <div
+            key={url}
+            id={`item${i}`}
+            className="carousel-item relative w-full overflow-hidden"
+          >
+            <img src={url} className="relative h-52 w-52" alt={`file${i}`} />
+            <button
+              className="relative bottom-20 right-9"
+              onClick={() => {
+                setPreviewImgs(previewImgs.filter((el) => el !== url));
+              }}
             >
-              <img src={url} className="absolute h-52 w-52" alt={`file${i}`} />
-              <button
-                className="absolute right-2 top-3 h-8 w-8"
-                onClick={() => {
-                  setPreviewImgs(previewImgs.filter((el) => el !== url));
-                }}
-              >
-                {/* <img
+              {/* <img
                   src="/svg/deleteIcon.svg"
                   alt="deleteIcon"
                   className="z-20 h-7 w-7 max-w-12"
                 /> */}
-                <svg
-                  width="24"
-                  height="24"
-                  viewBox="0 0 20 20"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    clipRule="evenodd"
-                    d="M17.3088 1.22279C17.3094 1.22297 17.3099 1.22325 17.3109 1.22429L18.7757 2.68907C18.7768 2.69012 18.7771 2.6906 18.7772 2.69121C18.7774 2.69178 18.7774 2.69239 18.7772 2.69296C18.7771 2.69354 18.7768 2.69403 18.7757 2.69507L11.4708 10L18.7757 17.305C18.7768 17.306 18.7771 17.3065 18.7772 17.3071C18.7774 17.3077 18.7774 17.3083 18.7772 17.3089C18.7771 17.3094 18.7768 17.3099 18.7757 17.311L17.3109 18.7758C17.3099 18.7768 17.3094 18.7771 17.3088 18.7773C17.3082 18.7774 17.3076 18.7774 17.3071 18.7773C17.3065 18.7771 17.306 18.7768 17.305 18.7758L10 11.4708L2.69504 18.7758C2.69402 18.7768 2.69351 18.7771 2.69293 18.7773C2.69235 18.7774 2.69173 18.7774 2.69115 18.7773C2.69059 18.7771 2.69008 18.7768 2.68904 18.7758L1.22426 17.311C1.22322 17.3099 1.22294 17.3095 1.22276 17.3088C1.22259 17.3083 1.22259 17.3077 1.22276 17.3071C1.22294 17.3065 1.22322 17.306 1.22426 17.305L8.52919 10L1.22426 2.69507C1.22322 2.69405 1.22294 2.69354 1.22276 2.69296C1.22258 2.69238 1.22258 2.69176 1.22276 2.69118C1.22294 2.69062 1.22322 2.69012 1.22426 2.68907L2.68904 1.22429C2.69008 1.22325 2.69057 1.22297 2.69118 1.22279C2.69175 1.22262 2.69236 1.22262 2.69293 1.22279C2.69351 1.22297 2.69399 1.22325 2.69504 1.22429L10 8.52922L17.305 1.22429C17.306 1.22325 17.3065 1.22297 17.3071 1.22279C17.3076 1.22261 17.3083 1.22261 17.3088 1.22279H17.3088Z"
-                    fill="black"
-                    stroke="white"
-                  />
-                </svg>
-              </button>
-            </div>
-          ))}
-          <div
-            className={`${previewImgs.length > 0 ? "hidden" : ""} carousel-item flex h-full w-full flex-col items-center justify-center gap-1 bg-subLightGray text-base text-subGray`}
-          >
-            사진 추가
-            <div className="text-xs">1MB 미만의 파일만 업로드 가능합니다</div>
-            <div className="text-xs">(최대 5장까지 첨부 가능)</div>
+              <svg
+                width="24"
+                height="24"
+                viewBox="0 0 20 20"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M17.3088 1.22279C17.3094 1.22297 17.3099 1.22325 17.3109 1.22429L18.7757 2.68907C18.7768 2.69012 18.7771 2.6906 18.7772 2.69121C18.7774 2.69178 18.7774 2.69239 18.7772 2.69296C18.7771 2.69354 18.7768 2.69403 18.7757 2.69507L11.4708 10L18.7757 17.305C18.7768 17.306 18.7771 17.3065 18.7772 17.3071C18.7774 17.3077 18.7774 17.3083 18.7772 17.3089C18.7771 17.3094 18.7768 17.3099 18.7757 17.311L17.3109 18.7758C17.3099 18.7768 17.3094 18.7771 17.3088 18.7773C17.3082 18.7774 17.3076 18.7774 17.3071 18.7773C17.3065 18.7771 17.306 18.7768 17.305 18.7758L10 11.4708L2.69504 18.7758C2.69402 18.7768 2.69351 18.7771 2.69293 18.7773C2.69235 18.7774 2.69173 18.7774 2.69115 18.7773C2.69059 18.7771 2.69008 18.7768 2.68904 18.7758L1.22426 17.311C1.22322 17.3099 1.22294 17.3095 1.22276 17.3088C1.22259 17.3083 1.22259 17.3077 1.22276 17.3071C1.22294 17.3065 1.22322 17.306 1.22426 17.305L8.52919 10L1.22426 2.69507C1.22322 2.69405 1.22294 2.69354 1.22276 2.69296C1.22258 2.69238 1.22258 2.69176 1.22276 2.69118C1.22294 2.69062 1.22322 2.69012 1.22426 2.68907L2.68904 1.22429C2.69008 1.22325 2.69057 1.22297 2.69118 1.22279C2.69175 1.22262 2.69236 1.22262 2.69293 1.22279C2.69351 1.22297 2.69399 1.22325 2.69504 1.22429L10 8.52922L17.305 1.22429C17.306 1.22325 17.3065 1.22297 17.3071 1.22279C17.3076 1.22261 17.3083 1.22261 17.3088 1.22279H17.3088Z"
+                  fill="black"
+                  stroke="white"
+                />
+              </svg>
+            </button>
           </div>
-        </div>
-        <div className="flex w-full justify-center gap-3 pb-2 pt-2">
-          {previewImgs.map((url, i) => (
-            <a
-              href={`#item${i}`}
-              className="btn btn-xs w-6 rounded-xl"
-              key={url}
-            >
-              {i + 1}
-            </a>
-          ))}
-        </div>
-      </label>
+        ))}
+      </div>
+      <div className="flex w-full justify-center gap-3 pb-2 pt-2">
+        {previewImgs.map((url, i) => (
+          <a href={`#item${i}`} className="btn btn-xs w-6 rounded-xl" key={url}>
+            {i + 1}
+          </a>
+        ))}
+      </div>
     </div>
   );
 };
