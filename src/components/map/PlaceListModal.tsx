92e854f1ef166a8a813e7005ba0c1bb3f9aeaf3a
yoonsoohwa <blue.yoonsh01@gmail.com>
2024-05-10 04:36:58 +0900

    style: placeListModal style 수정

--- src/components/map/PlaceListModal.tsx
+++ src/components/map/PlaceListModal.tsx
@@ -26,31 +26,32 @@ export default function PlaceListModal({
   return (
     <div>
       <div
-        className={`fixed z-10 h-[95.3vh] w-full bg-[#F7F7F9] pt-24 transition-all duration-300 ${openListModal ? "top-0" : "top-[1000px]"}`}
+        className={`fixed z-10 h-[95.3vh] w-full bg-[#F7F7F9] transition-all duration-300 ${openListModal ? "top-0" : "top-[1000px]"}`}
       >
-        <div
-          className={`fixed transition-all duration-300 ${openListModal ? "top-0" : "top-[1000px]"} z-10 h-24 w-full bg-[#F7F7F9]`}
-        ></div>
-        {placeList.length !== 0 && (
-          <div className="relative mx-auto my-0 w-[320px] pb-px text-right">
-            <Select
-              optionName={
-                userLocation.isSetUserLocation
-                  ? ["추천순", "별점순", "거리순"]
-                  : ["추천순", "별점순"]
-              }
-              optionValue={
-                userLocation.isSetUserLocation
-                  ? ["default", "rating", "distance"]
-                  : ["default", "rating"]
-              }
-              handler={handleSetFilter}
-            />
-          </div>
-        )}
-
         <PerfectScrollar>
-          <div className="relative mx-auto my-0 flex w-[320px] flex-col gap-3 pb-11 pt-px">
+          <div
+            className={`fixed left-1/2 -translate-x-1/2 transition-all duration-300 ${openListModal ? "top-0" : "top-[1000px]"} z-10 h-32 w-[330px] bg-[#F7F7F9]`}
+          >
+            {placeList.length !== 0 && (
+              <div className="relative mx-auto my-0 w-[320px] pb-px pt-24 text-right">
+                <Select
+                  optionName={
+                    userLocation.isSetUserLocation
+                      ? ["추천순", "별점순", "거리순"]
+                      : ["추천순", "별점순"]
+                  }
+                  optionValue={
+                    userLocation.isSetUserLocation
+                      ? ["default", "rating", "distance"]
+                      : ["default", "rating"]
+                  }
+                  handler={handleSetFilter}
+                />
+              </div>
+            )}
+          </div>
+
+          <div className="relative mx-auto my-0 flex w-[320px] flex-col gap-3 pb-4 pt-[8.5rem]">
             {placeList.length === 0 ? (
               //맛집이 없는 경우 메세지로 알림 추가
               <div className="mt-44 flex flex-col items-center gap-3">
